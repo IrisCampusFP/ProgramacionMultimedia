91ff7d9ba02b6e6953dd5ba7b9d0a2a2f73114ba
IrisCampusFP <iris.perezaparicio24@campusfp.es>
2026-02-15 15:08:42 +0100

    FlappyBird hecho en clase

---
+++ FlappyBirdClase/FlappyBird_IrisPerez/Assets/Scripts/Data/MyData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class MyData 
+{
+    [Serializable]
+    public struct PlayerStats
+    {
+        public int score;
+        public string name;
+
+    }
+
+    [SerializeField]
+    public PlayerStats stats;
+}
---
+++ FlappyBirdClase/FlappyBird_IrisPerez/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1.0f;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
